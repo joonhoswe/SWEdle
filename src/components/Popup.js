732117e76312526cf9f8714932332e42d5068d41
joonhoswe <joonhoswe@gmail.com>
2024-02-19 23:48:40 -0500

    added how to play popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react"
+import "../index.css"
 
 function Popup(props) {
   return (props.trigger) ? (
-    <div className='popup'>
-        <div className='popup-inner'>
-            <button onClick = {() => props.setTrigger(false)} className='close-btn'> Close </button>
+    <div>
+        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-700 w-1/3 h-3/4 p-4 shadow-lg rounded-lg">
+            <button onClick={() => props.setTrigger(false)} className="absolute top-3 right-3 bg-red-500 text-white transition duration-300 ease-in-out hover:bg-red-600 hover:scale-110 p-2 rounded-md">  
+            Close 
+            </button>
             {props.children}
         </div>
     </div>
