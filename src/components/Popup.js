acd7041b62491f29bb0e0d636571b04465881dfe
joonhoswe <joonhoswe@gmail.com>
2024-02-20 00:02:38 -0500

    updates styling of popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,7 +4,7 @@ import "../index.css"
 function Popup(props) {
   return (props.trigger) ? (
     <div>
-        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-700 w-1/3 h-3/4 p-4 shadow-lg rounded-lg">
+        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-700 w-1/3 h-4/5 p-4 shadow-lg rounded-lg">
             <button onClick={() => props.setTrigger(false)} className="absolute top-3 right-3 bg-red-500 text-white transition duration-300 ease-in-out hover:bg-red-600 hover:scale-110 p-2 rounded-md">  
             Close 
             </button>
