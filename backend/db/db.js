a953af381a194cca01e1a2724c240a8a9ff90f19
joonhoswe <joonhoswe@gmail.com>
2024-03-11 21:56:13 -0400

    setting up mongodb

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,4 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/?retryWrites=true&w=majority`;
 
 
@@ -11,16 +12,29 @@ const client = new MongoClient(uri, {
   }
 });
 
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
+    await  listDatabases(client);
+
+  } catch (e) {
+    console.error(e);
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
+
+
 run().catch(console.dir);
